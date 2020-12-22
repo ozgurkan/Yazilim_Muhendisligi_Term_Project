f4f2e044efb3d0c9291bed6bb9f7442349491a16
özgür kan <galatasaray_ozgur_94@hotmail.com>
2020-12-22 22:07:45 +0300

    first commit

--- DilKursuOtomasyonu/Form1.cs
+++ DilKursuOtomasyonu/Form1.cs
@@ -24,7 +24,7 @@ namespace DilKursuOtomasyonu
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            //
         }
 
         private void button1_Click(object sender, EventArgs e)
